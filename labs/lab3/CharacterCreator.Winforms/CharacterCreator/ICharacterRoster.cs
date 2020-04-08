cdb38c6dade940a6e07b3ef86c66aae0b1c7f9fc
William Sarawichitr <william.sarawichitr@my.tccd.edu>
2020-04-07 19:29:18 -0500

    lab

---
+++ labs/lab3/CharacterCreator.Winforms/CharacterCreator/ICharacterRoster.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterCreator
+{
+    public interface ICharacterRoster
+    {
+        Character Add ( Character character );
+
+        void Delete ( int id );
+
+        Character Get ( int id );
+
+        IEnumerable<Character> GetAll ();
+
+        void Update ( int id, Character character );
+    }
+}
